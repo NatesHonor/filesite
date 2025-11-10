b4ab2935544fd7fab244b3d7bead7f749744b37c
NatesHonor <nathaniel.j.marcellus@gmail.com>
2025-11-10 12:21:57 -0500

    Fixed isssue with dev api being called

--- src/pages/subpages/MissionchiefX.js
+++ src/pages/subpages/MissionchiefX.js
@@ -14,7 +14,7 @@ const MissionChiefX = () => {
     const fetchVersions = async () => {
       setLoading(true);
       try {
-        const response = await fetch(`http://localhost:5000/version/missionchiefbotx`, {
+        const response = await fetch(`https://api.natemarcellus.com/version/missionchiefbotx`, {
                     credentials: 'include',
 
         });
